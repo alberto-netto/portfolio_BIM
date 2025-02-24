2368e2b3f1aa5259c0b8793e74911fc49d5b2695
alberto-netto <110934689+alberto-netto@users.noreply.github.com>
2025-02-24 11:44:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,24 +19,33 @@ document.addEventListener("DOMContentLoaded", function() {
     function ajustarLayout() {
         const container = document.getElementById("container");
         container.innerHTML = ""; // Limpa o container antes de recriar os projetos
-
+    
         const projetos = [
-            { nome: "Projeto Casa do Lago", descricao: "Projeto de treinamento da Growarq", imagem: "assets/Casa do lago.png", link: "https://autode.sk/3QkICWu" },
+            { nome: "Projeto Casa do Lago", descricao: "Projeto de treinamento da Growarq", imagem: "assets/Casa do lago.png", link: "https://autode.sk/3QkICWu", link_e: "https://autode.sk/4keX9kg" },
             { nome: "Predio Residencial de 10 pavimentos", descricao: "Arquitetura básica e projeto elétrico de um prédio com apartamentos típicos, garagem e cobertura", imagem: "assets/Prédio com 10 pavimentos.png", link: "https://autode.sk/4k177Fu" },
             { nome: "Gerador Sustentável para Praça - Presidente Kenneddy - ES", descricao: "Projeto Conceitual de uma estrutura de gerador fotovoltaico utilizando os telhados de estruturas públicas e projeto de telhados solares de estacionamento ao redor da praça", imagem: "assets/Carport Kennedy.jpg", link: "https://autode.sk/4i4Uckb"},
         ];
-
+    
         projetos.forEach(projeto => {
             const divProjeto = document.createElement("div");
             divProjeto.classList.add("projeto");
-
-            divProjeto.innerHTML = `
+    
+            // Cria o HTML base do projeto
+            let projetoHTML = `
                 <h2>${projeto.nome}</h2>
                 <p>${projeto.descricao}</p>
                 <img src="${projeto.imagem}" alt="Imagem do ${projeto.nome}" class="exemplo-imagem">
-                <a href="${projeto.link}" target="_blank" class="botao-link">Acessar Projeto 3D</a>
+                <a href="${projeto.link}" target="_blank" class="botao-link">Acessar Projeto 3D - Arquitetura</a>
             `;
-
+    
+            // Adiciona o botão de projeto elétrico apenas se existir um link_e
+            if (projeto.link_e) {
+                projetoHTML += `
+                    <a href="${projeto.link_e}" target="_blank" class="botao-link">Acessar Projeto 3D - Elétrica</a>
+                `;
+            }
+    
+            divProjeto.innerHTML = projetoHTML;
             container.appendChild(divProjeto);
         });
     }
